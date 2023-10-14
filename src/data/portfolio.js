035c4a5121bc4d82330e4d179baeb146db5aa585
Ankit Bhawsar <bhawsarankit10@gmail.com>
2023-10-14 14:19:04 -0700

    update portfolio list

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,5 +1,24 @@
 export default [
     {
+        title: 'Natural Language AI Companion Chrome Extension',
+        domain: 'LLM',
+        date: 'October 2023',
+        img_Url: './assets/chatbot.JPG',
+        stack: ['LLM','JavaScript','OpenAI API'],
+        link: 'https://github.com/abhawsar10/Bhaws-Chatbot-Extension',
+        details: ["Developed a Chrome extension for Saatva.com, enhancing user experience and simplifying e-commerce interactions.","Integrated OpenAI's Language Model API to enable natural language queries, commands, and real-time content scraping for up-to-date product information.","Designed and implemented the extension's architecture, combining content scripts and a Node.js backend to ensure optimal performance and responsiveness."]
+    },
+    {
+        title: 'BHAWS Flix Website',
+        domain: 'Web Technologies',
+        date: 'March 2021',
+        img_Url: './assets/bhaws.jpg',
+        stack: ['Angular','NodeJS','Express'],
+        link: 'https://bhaws-flix.uw.r.appspot.com/',
+        // link: {"Link":'https://bhaws-flix.uw.r.appspot.com/',"Github":'https://github.com/abhawsar10/BHAWS-Flix-Website'},
+        details: ["Developed a web application using Angular, Bootstrap, and HTML5 that showcases the latest movies and TV shows.","It allows users to watch trailers and receive recommendations based on their selected movies.","Utilized the TMDB API to fetch movie, TV, and actor data and integrated YouTube's API to display trailers.","Implemented a Watch-list feature using the Web Storage API, enabling users to manage their personal list of movies and TV shows.","Created the backend using ExpressJS and hosted the application on the Google Cloud platform."]
+    },
+    {
         title: 'Transit Route Optimization using Uber/Lyft Data',
         domain: 'Machine Learning, Optimization',
         date: 'February 2023',
@@ -9,13 +28,13 @@ export default [
         details: ["Optimizing routing of transit services in and around the greater Boston Area using various machine learning models.","The project began with processing the data from rideshare records from Uber and Lyft.","Machine learning models included demand forecasting using ARIMA and SARIMAX and XGBoost.","Performed customer segmentation using K-means clustering to determine city hotspots.","Used Gurobi Optimization on UFLP model to plan efficient city transit routes."]
     },
     {
-        title: 'Case Study: Profit Optimization for Real Estate Construction',
-        domain: 'Operations Research',
-        date: 'November 2022',
-        img_Url: './assets/milp.jpg',
-        stack: ['Gurobi','AMPL'],
-        link: 'https://github.com/abhawsar10/Optimization-Analytics/tree/main/Case%20Study%201',
-        details: ["Mixed Integer Linear Programming Project to maximize profit for Lake Saddleback Development Corporation on 300 acres of land.","The problem involved dozens of variables and conditions to consider before optimizing for profit.","Formulated the problem and ran it in AMPL to optimize the number and types of various floor plans that can be constructed.","Conducted sensitivity analysis and presented a detailed report on how modifying each constraint and variable would affect the profits."]
+        title: 'Object Detection using SIFT Descriptors',
+        domain: 'Computer Vision',
+        date: 'October 2021',
+        img_Url: './assets/sift.jpg',
+        stack: ['Python','OpenCV','Google Colab'],
+        link: 'https://github.com/abhawsar10/Computer-Vision/tree/3b0668ae1477f1e5fe3b1a4b1105a6fd1dfc1cab/Object%20Detection%20using%20SIFT%20Descriptors',
+        details: ["Created and computed Scale Invariant Feature Transform (SIFT) descriptors for a dataset of images containing specific objects (books), which were used to estimate the homography matrix.","Applied a brute-force matcher to detect specific objects (books) within complex still life images."]
     },
     {
         title: 'Yelp Business Closure Predictions ',
@@ -54,15 +73,6 @@ export default [
         details: ["Constructed a LeNet-5 style Convolutional Neural Network (Modified from the 1998 paper), using PyTorch functions.","Used STL-10 Dataset for training containing 5000 96x96 RGB images","Conducted 2 Experiments with variantions: first with L2 Regularization, second with Batch Normalization","Experiments with Regularization showed a decrease in both Training and Testing Accuracies."]
     },
     {
-        title: 'Object Detection using SIFT Descriptors',
-        domain: 'Computer Vision',
-        date: 'October 2021',
-        img_Url: './assets/sift.jpg',
-        stack: ['Python','OpenCV','Google Colab'],
-        link: 'https://github.com/abhawsar10/Computer-Vision/tree/3b0668ae1477f1e5fe3b1a4b1105a6fd1dfc1cab/Object%20Detection%20using%20SIFT%20Descriptors',
-        details: ["Created and computed Scale Invariant Feature Transform (SIFT) descriptors for a dataset of images containing specific objects (books), which were used to estimate the homography matrix.","Applied a brute-force matcher to detect specific objects (books) within complex still life images."]
-    },
-    {
         title: 'Bouding Box Generation using Selective Search',
         domain: 'Computer Vision',
         date: 'October 2021',
@@ -71,24 +81,14 @@ export default [
         link: 'https://github.com/abhawsar10/Computer-Vision/tree/3b0668ae1477f1e5fe3b1a4b1105a6fd1dfc1cab/Selective%20Search%20Algorithm',
         details: ["Experimented with Selective Search Algorithms for bounding box generation","Applied 2 Stategies: Firstly, using image colour only, and second using all four similarities (color, texture, size, fill)."]
     },
-    // {
-    //     title: 'USC Films - Android App',
-    //     domain: 'Web Technologies',
-    //     date: 'April 2021',
-    //     img_Url: './assets/placeholder.jpg',
-    //     stack: ['Java','Android SDK'],
-    //     link: 'https://github.com/abhawsar10/Android-Dev/tree/main/USC%20Films%20App',
-    //     details: ["Developed an Android application version of the USC Films Website using Java and Android Studio.","The goal was to provide an optimized and native user experience on mobile devices."]
-    // },
     {
-        title: 'BHAWS Flix Website',
-        domain: 'Web Technologies',
-        date: 'March 2021',
-        img_Url: './assets/bhaws.jpg',
-        stack: ['Angular','NodeJS','Express'],
-        link: 'https://bhaws-flix.uw.r.appspot.com/',
-        // link: {"Link":'https://bhaws-flix.uw.r.appspot.com/',"Github":'https://github.com/abhawsar10/BHAWS-Flix-Website'},
-        details: ["Developed a web application using Angular, Bootstrap, and HTML5 that showcases the latest movies and TV shows.","It allows users to watch trailers and receive recommendations based on their selected movies.","Utilized the TMDB API to fetch movie, TV, and actor data and integrated YouTube's API to display trailers.","Implemented a Watch-list feature using the Web Storage API, enabling users to manage their personal list of movies and TV shows.","Created the backend using ExpressJS and hosted the application on the Google Cloud platform."]
+        title: 'Case Study: Profit Optimization for Real Estate Construction',
+        domain: 'Operations Research',
+        date: 'November 2022',
+        img_Url: './assets/milp.jpg',
+        stack: ['Gurobi','AMPL'],
+        link: 'https://github.com/abhawsar10/Optimization-Analytics/tree/main/Case%20Study%201',
+        details: ["Mixed Integer Linear Programming Project to maximize profit for Lake Saddleback Development Corporation on 300 acres of land.","The problem involved dozens of variables and conditions to consider before optimizing for profit.","Formulated the problem and ran it in AMPL to optimize the number and types of various floor plans that can be constructed.","Conducted sensitivity analysis and presented a detailed report on how modifying each constraint and variable would affect the profits."]
     },
     {
         title: "Game Playing AI agent for 'Go'",
@@ -99,6 +99,15 @@ export default [
         link: 'https://github.com/abhawsar10/-Go-Playing-AI-Agent',
         details: ["Developed game-playing agents capable of competing in the board game 'Go' using Artificial Intelligence techniques.","Implemented two different techniques: Utilized Minimax with Alpha-Beta pruning to play against amateur and random opponents.","Employed Q-Learning to compete against competition-level opponents.","Incorporated self-devised heuristics based on research papers to train bots using Q-Learning."]
     },
+    // {
+    //     title: 'USC Films - Android App',
+    //     domain: 'Web Technologies',
+    //     date: 'April 2021',
+    //     img_Url: './assets/placeholder.jpg',
+    //     stack: ['Java','Android SDK'],
+    //     link: 'https://github.com/abhawsar10/Android-Dev/tree/main/USC%20Films%20App',
+    //     details: ["Developed an Android application version of the USC Films Website using Java and Android Studio.","The goal was to provide an optimized and native user experience on mobile devices."]
+    // },
     {
         title: "Word Sense Disambiguation (WSD) using Neural Networks",
         domain: 'Natural Language Processing',
