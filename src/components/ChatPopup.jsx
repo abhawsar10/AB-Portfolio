fd1873a0bbfd229c372a6ab5e0d648d872acc1bd
Ankit Bhawsar <bhawsarankit10@gmail.com>
2023-10-26 16:58:31 -0700

    Added Chatbot

--- src/components/ChatPopup.jsx
+++ src/components/ChatPopup.jsx
@@ -1,44 +1,156 @@
 // ChatPopup.js
 import React, { useState } from 'react';
+import aboutAnkit from '../data/about';
+
+const sendicon = (
+    <svg xmlns="http://www.w3.org/2000/svg" height="1em" className='fill-white' viewBox="0 0 512 512">
+        <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/>
+    </svg>
+);
+
+const loading = (
+    <svg xmlns="http://www.w3.org/2000/svg" height="1em" className='fill-white spin' viewBox="0 0 512 512">
+        <path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/>
+    </svg>
+);
 
 const ChatPopup = ({ onClose }) => {
-  const [messages, setMessages] = useState([]);
-  const [currentMessage, setCurrentMessage] = useState("");
 
-  const handleSendMessage = () => {
-    if (currentMessage.trim()) {
-      setMessages([...messages, { text: currentMessage, sender: 'user' }]);
-      setCurrentMessage("");
-      // For demonstration, let's auto-reply after a user sends a message.
-      setTimeout(() => {
-        setMessages([...messages, { text: currentMessage, sender: 'user' }, { text: "Thanks for your message!", sender: 'bot' }]);
-      }, 1000);
+    const savedMessages = localStorage.getItem("chatMessages");
+    const initialMessages = savedMessages ? JSON.parse(savedMessages) : [];
+
+    const [messages, setMessages] = useState(initialMessages);
+    const [currentMessage, setCurrentMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter' && !event.shiftKey) {
+          event.preventDefault();
+          handleSendMessage();
+        }
+    };
+
+
+    const handleSendMessage = async () => {
+
+        if (currentMessage.trim()) {
+
+            setMessages([...messages, { text: currentMessage, sender: 'user' }]);
+
+            setCurrentMessage("");
+            setIsLoading(true);
+
+            let botResponse = ""
+            botResponse = await sendUserMessageToAPI(currentMessage, JSON.stringify(messages));         
+            console.log(botResponse)
+
+            const newMessages = [...messages, { text: currentMessage, sender: 'User' }, { text: botResponse, sender: 'AnkitBot' }];
+            setMessages(newMessages);
+            setIsLoading(false);
+
+            localStorage.setItem("chatMessages", JSON.stringify(newMessages));
+
+        }
     }
-  }
+    
+        const handleClearChat = () => {
+            setMessages([]);
+            localStorage.removeItem("chatMessages");
+        }
+
 
   return (
-    //   <div className="fixed top-40 md:right-20 p-2 m-2 rounded-xl z-50 bg-stone-500">
-    <div className="fixed top-40 md:right-20 p-2 m-2 w-11/12 sm:w-full md:w-auto mx-auto md:mx-0 rounded-xl z-50 bg-stone-500">
+    <>
+        <div className="fixed top-32 p-2 m-2 rounded-tl-xl rounded-bl-xl rounded-br-xl  z-50 bg-violet-300 dark:bg-orange-300
+             right-10 left-10 sm:right-24 sm:left-auto
+             sm:w-1/2 md:max-w-md lg:max-w-sm"
+        >
+            <div className=' flex justify-end'>
+                {/* <div className='fixed top-40 border-black border-2'>
 
-        <div className="border p-4 h-48 overflow-y-auto">
-          {messages.map((message, index) => (
-            <div key={index} className={`text-left ${message.sender === 'bot' ? 'text-gray-400' : 'text-black'}`}>
-              {message.text}
+                </div> */}
             </div>
-          ))}
+            <div className=" p-1 h-96 overflow-y-auto  rounded-tl-xl rounded-bl-xl rounded-br-xl   bg-stone-200">
+                <div className='text-center p-1'>
+                    <button className="rounded-xl px-4 py-1 bg-blue-500 dark:bg-green-700 text-white text-xs" onClick={handleClearChat}>Clear Chat</button>
+                </div>
+                <div className=" text-left text-white rounded-bl-xl rounded-br-xl m-2 p-2 bg-violet-300 dark:bg-orange-300 rounded-tl-xl">
+                    Hello! I am Ankit's Virtual Presence. I can answer any questions you may have about Ankit.
+                </div>
+                {messages.map((message, index) => (
+                    <div 
+                        key={index} 
+                        className={`
+                            text-left text-white rounded-bl-xl rounded-br-xl m-2 p-2 
+                            ${message.sender === 'AnkitBot' ? 'bg-violet-300 dark:bg-orange-300' : 'bg-blue-500 dark:bg-green-700'}
+                            ${message.sender === 'AnkitBot' ? 'rounded-tl-xl' : 'rounded-tr-xl'}
+                        `}
+                    >
+                        {message.text}
+                    </div>
+                ))}
+            </div>
+            <div className="mt-2 flex">
+                <input 
+                    type="text"
+                    value={currentMessage}
+                    onChange={(e) => setCurrentMessage(e.target.value)}
+                    onKeyDown={handleKeyPress} 
+                    className="border p-2 flex-grow rounded-lg"
+                    placeholder="Type your message..."
+                />
+                <button onClick={handleSendMessage} className="ml-2 w-12 flex items-center justify-center p-2 flex-none bg-blue-500 dark:bg-green-700 text-white rounded-lg">    
+                    <div id="normal-icon" className={isLoading ? 'hidden' : ''}>{sendicon}</div>
+                    <div id="loading-icon" className={isLoading ? '' : 'hidden'}>{loading}</div>
+                </button>
+            </div>  
         </div>
-        <div className="mt-4 flex">
-          <input 
-            type="text"
-            value={currentMessage}
-            onChange={(e) => setCurrentMessage(e.target.value)}
-            className="border p-2 flex-grow"
-            placeholder="Type your message..."
-          />
-          <button onClick={handleSendMessage} className="ml-2 p-2 bg-blue-500 text-white">Send</button>
-        </div>
-     </div>
+     </>
   );
+}
+
+
+async function sendUserMessageToAPI(userMessage,prev_messages) {
+
+    let prompt = "";
+
+    prompt += "You are the virtual presence of a professional software engineer named Ankit Bhawsar \n";
+    prompt += "You are present on his professional portfolio website \n";
+    prompt += "The User will ask professional and personal questions about Ankit Bhawsar\n";
+    prompt += "Answer the questions about Ankit as if you are him, based on the context and user query\n";
+
+    prompt += "Context about Ankit:\n";
+
+
+    prompt += aboutAnkit;
+
+    prompt += prev_messages + "\n";
+    prompt += "\n" + "user:" +userMessage;
+    
+    prompt += "Answer in 100 words\n";
+
+    console.log("prompt===>\n",prev_messages)
+
+    try {
+        const response = await fetch('https://bhaws-chatbot-extension.uw.r.appspot.com/generateChatResponse', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ user_msg: prompt })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data.message;
+        } else {
+            console.error('Error sending message to API');
+            return 'Error occurred while processing your request.';
+        }
+    } catch (error) {
+        console.error('Error sending message to API', error);
+        return 'Error occurred while processing your request.';
+    }
 }
 
 export default ChatPopup;
---
+++ src/data/about.js
@@ -0,0 +1,240 @@
+const aboutAnkit = `
+Name: Ankit Bhawsar;
+Birthday: 03/19/1998;
+Age: 25;
+Born: Pune,Maharashtra, India;
+Email: bhawsarankit10@gmail.com, abhawsar@berkeley.edu;
+Website: https://ankitbhawsar.com/;
+Linkedin: https://www.linkedin.com/in/ankitbhawsar/;
+Github: https://github.com/abhawsar10;
+
+Resume:
+Available for work:  October 2023 onwards
+Years of work Experience: Approximately 1 Year
+
+Full Stack Software Developer, Backend Developer, Frontend Developer, Data Scientist, ML Engineer;
+
+I'm a software engineer with a strong foundation in web technologies and a deep enthusiasm for  Machine Learning, Computer Vision , and Data Analytics.
+Open to explore compelling full-time career prospects that align with my interests.
+
+EDUCATION AND COURSEWORK:
+
+University of California, Berkeley
+Master of Analytics | GPA: 3.76/4
+Berkeley, California, USA
+Coursework:
+Database Systems
+Applied Data Science
+Applied Data Science with Venture Applications
+Financial Engineering
+Supply Chain Management
+Risk Modeling, Simulation, and Data Analysis
+Optimization Analytics
+Applications in Data Analysis;
+
+University of Southern California
+Master of Science in Computer Science | GPA: 3.8/4
+Los Angeles, California, USA
+Coursework:
+Analysis of Algorithms
+Foundations of Artificial Intelligence
+Web Technologies;
+Machine Learning
+Advanced Computer Vision
+Robotics
+Information Retrieval
+
+Savitribai Phule Pune University
+Bachelor of Engineering: Information Technology | GPA: 9.4/10
+Pune, India
+Coursework:
+Business Analytics and Intelligence;
+Machine Learning
+Software Design and Modelling
+Cloud Computing
+Database Systems
+Software Design and Modelling
+Software Testing and QA
+Cloud Computing
+Software Engineering and Project Management
+
+
+Languages: Python, Java, C++, SQL, MongoDB, HTML, CSS, JavaScript, TypeScript, R;
+Frameworks: Django, NodeJS, Express, Angular, React, Bootstrap, Android SDK, Flask, Tailwind;
+Technologies: RESTful API, Jira, MySQL, PostgreSQL, Git, Docker, Firebase;
+AI/ML: LLM APIs, OpenCV, PyTorch, Pandas, SciKit, Keras, TensorFlow;
+Cloud: AWS (Lambda, Elastic Beanstalk, EC2, S3, RDS), GCP (Compute Engine, App Engine);
+
+PROFESSIONAL EXPERIENCE:
+
+Software Engineer (May 2023 - August 2023)
+kwikkart, Berkeley, CA
+
+Core Team Engineer at kwikkart, working on an AI-powered seamless checkout solution
+Collaborated with the CEO and CTO to revamp the backend architecture
+Deployed the application to AWS Elastic Beanstalk and managed 2TB of data
+Utilized AWS SNS and Firebase Messaging Service for automation
+Built relationships with high-profile clients like DoorDash, Shipt, and Point Pickup
+Teaching Assistant for Machine Learning (August 2021 - May 2022)
+University of Southern California, Los Angeles, CA
+
+TA/Course Producer for CSCI 567
+Led a team of 4, creating 3 Machine Learning programming assignments
+Counseled students, coordinated workshops, and mentored graders
+
+PROJECT EXPERIENCE:
+[
+    {
+        title: 'Natural Language AI Companion Chrome Extension',
+        domain: 'LLM',
+        date: 'October 2023',
+        img_Url: './assets/chatbot.JPG',
+        stack: ['LLM','JavaScript','OpenAI API'],
+        link: 'https://github.com/abhawsar10/Bhaws-Chatbot-Extension',
+        details: ["Developed a Chrome extension for Saatva.com, enhancing user experience and simplifying e-commerce interactions.","Integrated OpenAI's Language Model API to enable natural language queries, commands, and real-time content scraping for up-to-date product information.","Designed and implemented the extension's architecture, combining content scripts and a Node.js backend to ensure optimal performance and responsiveness."]
+    },
+    {
+        title: 'BHAWS Flix Website',
+        domain: 'Web Technologies',
+        date: 'March 2021',
+        img_Url: './assets/bhaws.jpg',
+        stack: ['Angular','NodeJS','Express'],
+        link: 'https://bhaws-flix.uw.r.appspot.com/',
+        // link: {"Link":'https://bhaws-flix.uw.r.appspot.com/',"Github":'https://github.com/abhawsar10/BHAWS-Flix-Website'},
+        details: ["Developed a web application using Angular, Bootstrap, and HTML5 that showcases the latest movies and TV shows.","It allows users to watch trailers and receive recommendations based on their selected movies.","Utilized the TMDB API to fetch movie, TV, and actor data and integrated YouTube's API to display trailers.","Implemented a Watch-list feature using the Web Storage API, enabling users to manage their personal list of movies and TV shows.","Created the backend using ExpressJS and hosted the application on the Google Cloud platform."]
+    },
+    {
+        title: 'Transit Route Optimization using Uber/Lyft Data',
+        domain: 'Machine Learning, Optimization',
+        date: 'February 2023',
+        img_Url: './assets/uber.JPG',
+        stack: ['Python','Keras','Pandas'],
+        link: 'https://github.com/abhawsar10/Machine-Learning-Projects/tree/main/Transit%20Route%20Optimization',
+        details: ["Optimizing routing of transit services in and around the greater Boston Area using various machine learning models.","The project began with processing the data from rideshare records from Uber and Lyft.","Machine learning models included demand forecasting using ARIMA and SARIMAX and XGBoost.","Performed customer segmentation using K-means clustering to determine city hotspots.","Used Gurobi Optimization on UFLP model to plan efficient city transit routes."]
+    },
+    {
+        title: 'Object Detection using SIFT Descriptors',
+        domain: 'Computer Vision',
+        date: 'October 2021',
+        img_Url: './assets/sift.jpg',
+        stack: ['Python','OpenCV','Google Colab'],
+        link: 'https://github.com/abhawsar10/Computer-Vision/tree/3b0668ae1477f1e5fe3b1a4b1105a6fd1dfc1cab/Object%20Detection%20using%20SIFT%20Descriptors',
+        details: ["Created and computed Scale Invariant Feature Transform (SIFT) descriptors for a dataset of images containing specific objects (books), which were used to estimate the homography matrix.","Applied a brute-force matcher to detect specific objects (books) within complex still life images."]
+    },
+    {
+        title: 'Yelp Business Closure Predictions ',
+        domain: 'Machine Learning',
+        date: 'November 2022',
+        img_Url: './assets/yelp.jpg',
+        stack: ['Python','Scikit-learn','Pandas'],
+        link: 'https://github.com/abhawsar10/Machine-Learning-Projects/tree/main/Yelp%20Business%20Prediction',
+        details: ["Developed a project to predict whether a given business listed on Yelp will close or continue operations in the coming years using Python, SciKit, and TensorFlow.","Utilized a publicly available dataset with approximately 150,000 observations.","Employed Natural Language Processing for Sentiment Analysis on business reviews.","Ran multiple machine learning models on the data, including Logistic Regression, LDA, CART, Random Forest, and Neural Networks, achieving an average accuracy of approximately 82% on the testing dataset.","Enhanced models using boosting techniques such as AdaBoost, XGBoost, and CatBoost, resulting in an average improvement of approximately 2%."]
+    },
+    {
+        title: 'Indexing Fox News Webpages using Solr',
+        domain: 'Information Retrieval',
+        date: 'March 2022',
+        img_Url: './assets/fox.JPG',
+        stack: ['Solr','Lucene','PHP'],
+        link: 'https://github.com/abhawsar10/Information-Retrieval/tree/main/Indexing%20using%20Solr',
+        details: ["Configured the Apache Solr server to index a set of 50,000 Fox News HTML webpages and enabled HTTPS requests to the server.","Implemented the PageRank algorithm using the networkx library to calculate ranks for all webpages.","Developed a front-end PHP search engine client to allow users to submit queries to both the PageRank and Lucene algorithms.","Discovered an average overlap of 6% in the results of ranking strategies used by Lucene and Google, indicating differences in techniques."]
+    },
+    {
+        title: 'Semantic Segmentation using Modified Fully Convolutional Networks',
+        domain: 'Computer Vision',
+        date: 'November 2021',
+        img_Url: './assets/semseg.png',
+        stack: ['Python','OpenCV','Google Colab'],
+        link: 'https://github.com/abhawsar10/Computer-Vision/tree/main/Semantic%20Segmentation%20using%20FCN',
+        details: ["Conducted research under Prof. Ram Nevatia, Director of IRIS Lab at USC, as part of a group of 3 PhD students.","Utilized a modified version of FCN32 and FCN16 with a ResNet-18 backbone for semantic segmentation of traffic dash cam images into 10 categories.","Trained the model on the Kitti Vision dataset, which consists of images captured from vehicles driving in busy traffic conditions.","Achieved a Mean Pixel-Level Intersection-over-Union (IoU) of 0.21, indicating the model's performance in semantic segmentation."]
+    },
+    {
+        title: 'Object Classification using Modified LeNet5',
+        domain: 'Computer Vision',
+        date: 'October 2021',
+        img_Url: './assets/lenet.png',
+        stack: ['Python','PyTorch','Google Colab'],
+        link: 'https://github.com/abhawsar10/Computer-Vision/tree/3b0668ae1477f1e5fe3b1a4b1105a6fd1dfc1cab/LeNet%205%20Implementation',
+        details: ["Constructed a LeNet-5 style Convolutional Neural Network (Modified from the 1998 paper), using PyTorch functions.","Used STL-10 Dataset for training containing 5000 96x96 RGB images","Conducted 2 Experiments with variantions: first with L2 Regularization, second with Batch Normalization","Experiments with Regularization showed a decrease in both Training and Testing Accuracies."]
+    },
+    {
+        title: 'Bouding Box Generation using Selective Search',
+        domain: 'Computer Vision',
+        date: 'October 2021',
+        img_Url: './assets/selective search.jpeg',
+        stack: ['Python','OpenCV','Google Colab'],
+        link: 'https://github.com/abhawsar10/Computer-Vision/tree/3b0668ae1477f1e5fe3b1a4b1105a6fd1dfc1cab/Selective%20Search%20Algorithm',
+        details: ["Experimented with Selective Search Algorithms for bounding box generation","Applied 2 Stategies: Firstly, using image colour only, and second using all four similarities (color, texture, size, fill)."]
+    },
+    {
+        title: 'Case Study: Profit Optimization for Real Estate Construction',
+        domain: 'Operations Research',
+        date: 'November 2022',
+        img_Url: './assets/milp.jpg',
+        stack: ['Gurobi','AMPL'],
+        link: 'https://github.com/abhawsar10/Optimization-Analytics/tree/main/Case%20Study%201',
+        details: ["Mixed Integer Linear Programming Project to maximize profit for Lake Saddleback Development Corporation on 300 acres of land.","The problem involved dozens of variables and conditions to consider before optimizing for profit.","Formulated the problem and ran it in AMPL to optimize the number and types of various floor plans that can be constructed.","Conducted sensitivity analysis and presented a detailed report on how modifying each constraint and variable would affect the profits."]
+    },
+    {
+        title: "Game Playing AI agent for 'Go'",
+        domain: 'Reinforcement Learning',
+        date: 'November 2020',
+        img_Url: './assets/Go.jpeg',
+        stack: ['Python','ML','Q Learning'],
+        link: 'https://github.com/abhawsar10/-Go-Playing-AI-Agent',
+        details: ["Developed game-playing agents capable of competing in the board game 'Go' using Artificial Intelligence techniques.","Implemented two different techniques: Utilized Minimax with Alpha-Beta pruning to play against amateur and random opponents.","Employed Q-Learning to compete against competition-level opponents.","Incorporated self-devised heuristics based on research papers to train bots using Q-Learning."]
+    },
+    {
+        title: 'USC Films - Android App',
+        domain: 'Web Technologies',
+        date: 'April 2021',
+        img_Url: './assets/placeholder.jpg',
+        stack: ['Java','Android SDK'],
+        link: 'https://github.com/abhawsar10/Android-Dev/tree/main/USC%20Films%20App',
+        details: ["Developed an Android application version of the USC Films Website using Java and Android Studio.","The goal was to provide an optimized and native user experience on mobile devices."]
+    },
+    {
+        title: "Word Sense Disambiguation (WSD) using Neural Networks",
+        domain: 'Natural Language Processing',
+        date: 'February 2020',
+        img_Url: './assets/word2vec.png',
+        stack: ['Python','WordNet','Keras'],
+        link: 'https://github.com/abhawsar10/Word-Sense-Disambiguation-using-Neural-Networks',
+        details: ["Developed Artificial Neural Networks for predicting word senses based on sentence context.","Used Word2Vec to create word embeddings for highly ambiguous words based on two datasets, OMSTI and SemCor, which together contained over one million sense-tagged instances of words based on WordNet senses.","Achieved over 80% accuracy on SemEval Tasks.","Presented a seminar at the International Conference on Pervasive Computing 2020 on Supervised Learning approaches to Word Sense Disambiguation (WSD)."]
+    },
+    {
+        title: "Airline Ticketing System",
+        domain: 'Database Management',
+        date: 'March 2019',
+        img_Url: './assets/airline.jpg',
+        stack: ['Java','MySQL','Python'],
+        link: 'https://github.com/abhawsar10/Java-MySQL-Airline-Ticketing-Project',
+        details: ["Developed software for booking airfare tickets online.","Created a backend database deployed on an Apache server using phpMyAdmin.","Implemented front-end database queries using Java.","Generated data to populate the databases using Python packages like pydbgen and faker.","The project achieved the 1st rank among a class of 160 students."]
+    }
+]
+
+ACTIVITIES
+
+Junior Engineer of the VEX Robotics team at USC (ranked 5th in the World)
+Cal Adventures Level 1 Licensed Sailor
+Active part of organizing team at Door Step School, an NGO involved in education for underdeveloped communities
+
+HOBBIES:
+Hiking: Hiked 12 different forts in Maharashtra, India; Hiked Angel's Landing in Zion National Park; Attempted and failed to climb Half Dome in Yosemite National Park;
+Hitting the gym;
+Badminton; 
+Basketball; 
+Football;
+Football Enthusiast: favorite team: Chelsea; 
+Football Enthusiast: favorite team: India; 
+Sailing;
+Painting/ Sketching/ Drawing;
+Cycling;
+Exploring new places;
+
+FUN FACTs:
+Visited 11 National parks in the USA;
+Coding since I was 11 years old;
+`;
+
+export default aboutAnkit;
