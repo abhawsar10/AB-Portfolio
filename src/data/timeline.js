1b0215e4af296a085bc41764ab9f84cb6eeb8c0c
Ankit Bhawsar <bhawsarankit10@gmail.com>
2024-02-05 23:03:57 -0800

    Updated Timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,7 +1,7 @@
 export default [
     {
         year: 'Today',
-        title: 'Software Engineer',
+        title: 'Founding Software Engineer',
         subtitle: 'kwikkart',
         img: './assets/kk.png',
         item_color: '#074D0E',
@@ -9,12 +9,18 @@ export default [
         duration: '4 months',
         // details: ['Leading the software team as the Head of Development, focused on implementing an AI-powered seamless checkout solution aimed at reducing in-store time by up to 60%.',' Revamped the backend architecture by migrating from SST library to Django, improving performance and flexibility.','Collaborated closely with the CEO and CTO to design the database schema and API calls, ensuring seamless integration across teams.',' Deployed the application to AWS Elastic Beanstalk, reducing deployment time by 70-80% and effectively managing 2TB of data storage on RDS.',' Integrated AWS SNS and Firebase Messaging Service for Lambda function-triggered communication between database, firmware, and mobile app components.',' Played a pivotal role in building relationships with high-profile clients such as DoorDash, Shipt, and Point Pickup, securing their participation in upcoming pilot programs.',' Actively involved in Berkeley Skydeck and contributed to positioning kwikkart as a promising player in the industry, leading to successful fundraising of $225k.']
         details: [
-            "Core Team Engineer at kwikkart, driving the end-to-end implementation of an AI-powered seamless checkout solution, aiming to reduce time in-store by up to 60%.",
-            "Collaborated closely with the CEO and CTO to revamp the backend architecture, cultivate the database schema, and design most API calls, ensuring seamless integration between the front-end, mobile, and firmware teams.",
-            "Deployed the application to AWS Elastic Beanstalk, achieving a 70-80% reduction in deployment time and establishing connections to a PostgreSQL database on RDS, effectively managing 2TB of data storage and retrieval.",
-            "Utilized AWS SNS and Firebase Messaging Service integration to automate communication between database, firmware, and mobile app components, with the assistance of Lambda Functions.",
-            "Contributed to the startup's growth trajectory, playing a key role in building relationships with high-profile clients like DoorDash, Shipt, and Point Pickup, securing their interest in participating in pilot programs."
+            "Part of founding team at KwikKart, driving the end-to-end implementation of a Smart-cart seamless checkout solution, aiming to reduce time in-store by up to 60%.",
+            "Actively contributed to the development of cart intrusion algorithms using OpenCV and Sensor Fusion, achieving an impressive 96% and 88% accuracies in detecting and classifying shopping cart activity, respectively.",
+            "Successfully built and demonstrated the Minimum Viable Product (MVP) at the Food Service Innovation Zone during NRF 2024, garnering significant interest from key industry players and potential partners such as Shipt, H-E-B, Sprouts, and Buncha.",
+            "Instrumental in the startup's growth trajectory, which led to KwikKart's acceptance into the TechStars New York City Accelerator program, securing pre-seed funding of $200k."
         ]
+        // details: [
+        //     "Core Team Engineer at kwikkart, driving the end-to-end implementation of an AI-powered seamless checkout solution, aiming to reduce time in-store by up to 60%.",
+        //     "Collaborated closely with the CEO and CTO to revamp the backend architecture, cultivate the database schema, and design most API calls, ensuring seamless integration between the front-end, mobile, and firmware teams.",
+        //     "Deployed the application to AWS Elastic Beanstalk, achieving a 70-80% reduction in deployment time and establishing connections to a PostgreSQL database on RDS, effectively managing 2TB of data storage and retrieval.",
+        //     "Utilized AWS SNS and Firebase Messaging Service integration to automate communication between database, firmware, and mobile app components, with the assistance of Lambda Functions.",
+        //     "Contributed to the startup's growth trajectory, playing a key role in building relationships with high-profile clients like DoorDash, Shipt, and Point Pickup, securing their interest in participating in pilot programs."
+        // ]
     },
     {
         year: '2023',
